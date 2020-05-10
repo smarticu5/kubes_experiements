68568fedc7ba117dbc06b4fb3da10177161841c2
Iain <iain@iainsmart.co.uk>
2020-05-10 12:46:16 +0100

    Add Tiller to cluster

--- Really_Vulnerable_Cluster/deploy.sh
+++ Really_Vulnerable_Cluster/deploy.sh
@@ -2,3 +2,9 @@ kind create cluster --config cluster_config.yaml
 
 # Create Dashboard
 kubectl apply -f kubernetes_dashboard.yaml
+
+# Add customisations
+kubectl apply -f cluster_manifest.yaml
+
+# Initialise Helm
+helm init
